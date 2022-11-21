b6a002e55b0f0f7233dc7b3deaec731e50d77430
bsmith86dev <bsmith86.dev@gmail.com>
2022-11-20 21:53:11 -0600

    first commit

---
+++ loopshw.js
@@ -0,0 +1,23 @@
+ //Write a for loop that logs 0-15;
+ 
+ 
+ for (let i = 0; i <= 10; i++ ) {
+    console.log(i);
+ }
+
+//Write a for loop that counts from 15-0;
+
+
+for (let i = 16; i >= 0; i-- ) {
+    console.log(i);
+ }
+
+
+ //Make variable laps = 0; write a for loop that logs (thats another lap!) and adds 1 to the laps variable. AFTER the loop, log laps.
+
+let i = "laps"; 
+
+ for (let i = 0; i >= 0; i++ ) {
+    console.log("that's another lap! " + i);
+ }
+
